bbd1cb8aa5bfa9e0bff5a1410265227164f3b735
Biljana Krstajic <biljana.web.development@gmail.com>
2024-06-28 08:47:32 +0200

    added Logout button

--- src/components/header/PageHeader.js
+++ src/components/header/PageHeader.js
@@ -1,16 +1,25 @@
+import { useContext } from "react";
+
+import LoginContext from "../../store/login-context";
+
 import AddButton from "./AddButton";
 import classes from "./PageHeader.module.css";
 
 function PageHeader() {
+  const ctx = useContext(LoginContext);
+
   return (
     <>
-      <nav className={classes.header}>
-        <ul>
-          <li>
-            <AddButton />
-          </li>
-        </ul>
-      </nav>
+      <header className={classes.header}>
+        <div>
+          <button className={classes.button} onClick={ctx.onLogout}>
+            Logout
+          </button>
+        </div>
+        <div>
+          <AddButton />
+        </div>
+      </header>
     </>
   );
 }
